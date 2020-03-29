5c0a80c3cb5208f261d82a01af4e44500684d28d
Jackson Henrique Hochscheidt <jackson.hochscheidt@gmail.com>
2020-03-29 12:56:47 -0300

    Problema com as rotas

---
+++ aulas/mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
---
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function Incidents() {
+  return (
+    <View style={styles.container} >
+    
+    </View>
+  );
+}
